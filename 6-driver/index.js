0640d7f3e115619ce847c1703c0947281dc8e841
juls <yulya29711997@icloud.com>
2024-12-31 15:30:49 +0700

    2

---
+++ 6-driver/index.js
@@ -0,0 +1,5 @@
+const hasLicence = true;
+const age = 18;
+const isDrunk = false;
+
+const result = (hasLicence) && (age >=18) && (!isDrunk) ? console.log('You can drive') : console.log('You can not drive:(')
